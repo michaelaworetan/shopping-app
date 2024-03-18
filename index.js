529d6933d8e517deaf525dabf87cdb10bf32060c
Aworetan Michael <mvillacs07@gmail.com>
2024-03-18 22:23:17 +0100

    connect to mongodb and added .env

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-// 1. import express 
+// 1. import packages 
 const express = require("express")
-
-const port = 3000  /**setting port number  */ 
+const mongoose = require('mongoose');
+const dotenv = require("dotenv")
 
 //2. creating express application
 const app = express()
+const port = 3000  /**setting port number  */
 
+//5. configuring dotenv
+dotenv.config()
+
+// 4. connecting to mongodb database (connection string gotten from mongodb compass)
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("mongodb connected!") 
+})
 
 // 3. listening for connections 
 app.listen(port, () => {
     console.log("Express server is running on port 3000");
-} )
+})
