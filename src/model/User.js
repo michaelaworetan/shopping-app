a3c2a3621e7fdba125d13cf15d09889741d41f39
Aworetan Michael <mvillacs07@gmail.com>
2024-03-20 15:57:44 +0100

    added verification properties to User model

--- src/model/User.js
+++ src/model/User.js
@@ -1,6 +1,6 @@
 //importing mongoose
 const mongoose = require("mongoose")
-
+const crypto = require("crypto")  //built-in module to generate verification token
 
 //Contains code for userSchema and User model creation
 // 6. creating userSchema
@@ -15,9 +15,12 @@ const userSchema = new mongoose.Schema({
         country: String,
     },
     phoneNumber: { type: String, required: true },
+    // properties for user verification
+    isVerified: { type: Boolean, default: false }, //any user that will be created won't be verified by default
+    verificationToken: { type: String, default: crypto.randomBytes(32).toString('hex') } //verify user we want it to be unique at all times using the crypto module
 })
 
 //7. creating a model from the userSchema
 const User = mongoose.model("User", userSchema) //make Usermodel available in the code so you can create users and query the Usermodel using moongose query functions (find, findByIdAndDelete)
 
-module.exports = User //expoerting the User and making it available to other parts of the project
+module.exports = User //exporting the User and making it available to other parts of the project
