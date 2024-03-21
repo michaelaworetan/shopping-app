5eca15f07bd3d51290760f7b4ff8b5e406353152
Aworetan Michael <mvillacs07@gmail.com>
2024-03-21 13:08:37 +0100

    Implemented mail verification sending 2

--- src/controller/User.js
+++ src/controller/User.js
@@ -1,7 +1,11 @@
 // User controller containing code for the route
-const User = require("../model/User")   //iomport User from model
-const bcrypt = require("bcrypt")  //password hashing
+const User = require("../model/User")   //import User from model
+const nodemailer = require('nodemailer')   //import nodemailer
+const dotenv = require('dotenv')   //importing dotenv 
+const bcrypt = require("bcrypt")  //password hashing  
 const saltRounds = 10 //adding salt (10 digit charater to make the app more secure)
+
+dotenv.config()  //allow us to save our value into process.env by default
 
 //8. To create a user we need to create a new route
 //post method because we are trying to create a new resource
@@ -22,14 +26,57 @@ let createUser = async(req, res) => {
             phoneNumber
         })
 
+        //11. function for sending mail
+        sendVerificationEmail(newUser) //passing the newUser as the parameter value
+
         //saving the new user in the database(save function on the newUser object)
         await newUser.save()
         //sending a response as a json object with newUser
         res.status(201).json(newUser)
-    } catch (error) {
+    } catch (error){
         console.error(error);
         //500(issue with server side code)
         res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+//12. creating nodemailer transporter
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {                             //authorization
+        user: "mvillacs07@gmail.com",
+        pass: process.env.MAIL_PASSWORD
+    }
+})
+
+//13. sendVericationEmail function definition
+let sendVerificationEmail = async (user) => {
+    //creating verification link from verification token
+    const verificationLink = `http://localhost:3000/user/verifyUser?token=${user.verificationToken}` //verifyUser endpoint
+
+    //setting up mail options(from, to and so on)
+    const mailOptions = {
+        from: "Shopping App <mvillacs07@gmail.com>", //hide the name of the gmail under the shopping app
+        to: user.email,
+        subject: "Verify your email address",
+        //Template for sending verification mails
+        text: `
+        Hello ${user.name},
+
+        Please click on the link below to verify your email address:
+        ${verificationLink}
+
+        If you didn't create an account, please disregard this email.
+
+        Thank you, 
+        The Shopping App team
+        `
+    }
+    //sending the mail using mailOptions
+    try {
+        transporter.sendMail(mailOptions)
+    } catch (error) {   //catch any error when sending mail
+        console.error(error);
     }
 }
 
